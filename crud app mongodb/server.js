8c3485f12bebc0c26fdf0190469d4b3debdbcd95
VIKRAM NETHA <165703977+VIKRAMNETHA@users.noreply.github.com>
2024-07-03 11:48:48 +0530

    Add files via upload

---
+++ crud app mongodb/server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+
+mongoose.connect('mongodb://localhost:27017/mydatabase', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const itemSchema = new mongoose.Schema({
+  name: String,
+  quantity: Number,
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+
+app.post('/items', async (req, res) => {
+  try {
+    const item = new Item(req.body);
+    await item.save();
+    res.status(201).send(item);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+
+app.get('/items', async (req, res) => {
+  try {
+    const items = await Item.find();
+    res.status(200).send(items);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
+app.put('/items/:id', async (req, res) => {
+  try {
+    const item = await Item.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+    if (!item) {
+      return res.status(404).send();
+    }
+    res.status(200).send(item);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+
+app.delete('/items/:id', async (req, res) => {
+  try {
+    const item = await Item.findByIdAndDelete(req.params.id);
+    if (!item) {
+      return res.status(404).send();
+    }
+    res.status(200).send(item);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
